e3e979f5ce0da945ae84ace33b888139e0265843
kanadeiar <kanadeiar@gmail.com>
2021-09-26 06:52:19 +0300

    Первый контроллер

---
+++ EasyBookStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace EasyBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
