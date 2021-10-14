2c5415f56a228ac2a9fcaea347995f9a973f72cd
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-14 10:07:42 +0400

    Перенос начальных данных в отдельный класс

--- EasyBookStore/Controllers/WorkerController.cs
+++ EasyBookStore/Controllers/WorkerController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using EasyBookStore.Models;
 using EasyBookStore.WebModels;
+using EasyBookStore.Models.Data;
 
 namespace EasyBookStore.Controllers
 {
     public class WorkerController : Controller
     {
-        private static readonly IList<Worker> __Workers = Worker.GetWorkers;
+        private static readonly IList<Worker> __Workers = StaticData.GetWorkers;
         public IActionResult Index()
         {
             return View(__Workers);
---
+++ EasyBookStore/Models/Data/StaticData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyBookStore.Models.Data
+{
+    public class StaticData
+    {
+        public static List<Worker> GetWorkers => Enumerable.Range(1, 30).Select(p => new Worker
+        {
+            Id = p,
+            FirstName = $"Иван {p}",
+            LastName = $"Иванов {p}",
+            Patronymic = $"Иванович {p}",
+            Age = p + 18,
+        }).ToList();
+    }
+}
