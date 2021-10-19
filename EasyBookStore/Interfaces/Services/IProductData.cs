40cb0006368f172ef675a3496b6a9685f6e4f4c5
kanadeiar <kanadeiar@gmail.com>
2021-10-20 00:29:52 +0400

    + Сервис каталога товаров

---
+++ EasyBookStore/Interfaces/Services/IProductData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyBookStore.Domain.Models;
+
+namespace EasyBookStore.Interfaces.Services
+{
+    public interface IProductData
+    {
+        /// <summary> Получить жанры книг </summary>
+        IEnumerable<Genre> GetGenres();
+        /// <summary> Получить авторов книг </summary>
+        IEnumerable<Author> GetAuthors();
+    }
+}
---
+++ EasyBookStore/Services/InMemoryProductData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyBookStore.Domain.Models;
+using EasyBookStore.Interfaces.Services;
+using EasyBookStore.Models.Data;
+
+namespace EasyBookStore.Services
+{
+    /// <summary> Сервис хранения данных по товарам в памяти </summary>
+    public class InMemoryProductData : IProductData
+    {
+        public IEnumerable<Genre> GetGenres()
+        {
+            return StaticData.Genres;
+        }
+
+        public IEnumerable<Author> GetAuthors()
+        {
+            return StaticData.Authors;
+        }
+    }
+}
