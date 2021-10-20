d585f0edd116a866e62bfcd85d2ceffc036e1bf1
kanadeiar <kanadeiar@gmail.com>
2021-10-21 00:27:11 +0400

    Визуальный компонент жанров книг работает успешно

---
+++ EasyBookStore/WebModels/GenreWebModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyBookStore.WebModels
+{
+    public class GenreWebModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public GenreWebModel Parent { get; set; }
+        public List<GenreWebModel> Child { get; set; } = new();
+    }
+}
