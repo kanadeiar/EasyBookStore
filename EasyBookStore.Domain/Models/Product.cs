48c07edbab50d82377e8654555d49ed424eb90b5
kanadeiar <kanadeiar@gmail.com>
2021-10-21 00:46:04 +0400

    Определение товаров

---
+++ EasyBookStore.Domain/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using EasyBookStore.Domain.Models.Base;
+using EasyBookStore.Domain.Models.Base.Interfaces;
+
+namespace EasyBookStore.Domain.Models
+{
+    /// <summary> Товар - книга </summary>
+    public class Product : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; }
+        /// <summary> Жанр </summary>
+        public int GenreId { get; set; }
+        /// <summary> Автор </summary>
+        public int? AuthorId { get; set; }
+        /// <summary> Изображение книги </summary>
+        public string ImageUrl { get; set; }
+        /// <summary> Стоимость книги </summary>
+        public decimal Price { get; set; }
+    }
+}
