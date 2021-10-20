9c100a37d522f13e845b1e47f9aacd6438b782df
kanadeiar <kanadeiar@gmail.com>
2021-10-21 00:36:23 +0400

    Секция с авторами книг нормально работает, показывается

--- EasyBookStore/Components/AuthorsViewComponent.cs
+++ EasyBookStore/Components/AuthorsViewComponent.cs
@@ -2,12 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EasyBookStore.Interfaces.Services;
+using EasyBookStore.WebModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EasyBookStore.Components
 {
     public class AuthorsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke() => View();
+        private readonly IProductData _productData;
+        public AuthorsViewComponent(IProductData productData)
+        {
+            _productData = productData;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var authors = _productData.GetAuthors().OrderBy(a => a.Order).Select(a => new AuthorWebModel
+            {
+                Id = a.Id,
+                Name = a.Name,
+                Order = a.Order,
+            });
+
+            return View(authors);
+        }
     }
 }
---
+++ EasyBookStore/WebModels/AuthorWebModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyBookStore.WebModels
+{
+    public class AuthorWebModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+    }
+}
