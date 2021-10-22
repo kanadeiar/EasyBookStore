f5bb70245675ffa989ba68004fc4b2ac974f3d62
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-22 08:33:50 +0400

    Создание контекста базы данных

---
+++ EasyBookStore.Dal/BookStoreContext.cs
@@ -0,0 +1,15 @@
+﻿using EasyBookStore.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyBookStore.Dal
+{
+    public class BookStoreContext : DbContext
+    {
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+
+    }
+}
