909c08adc685b6132c3a071eae4593bfaea39cf0
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-22 11:26:27 +0400

    Сервис каталога товаров в базе данных

---
+++ EasyBookStore/Services/Database/DatabaseProductData.cs
@@ -0,0 +1,34 @@
+﻿using EasyBookStore.Dal;
+using EasyBookStore.Domain.Common;
+using EasyBookStore.Domain.Models;
+using EasyBookStore.Interfaces.Services;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EasyBookStore.Services.Database
+{
+    public class DatabaseProductData : IProductData
+    {
+        private readonly BookStoreContext _context;
+        public DatabaseProductData(BookStoreContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Genre> GetGenres() => _context.Genres;
+        public IEnumerable<Author> GetAuthors() => _context.Authors;
+
+
+        public IEnumerable<Product> GetProducts(ProductFilter filter = null)
+        {
+            IQueryable<Product> query = _context.Products;
+
+            if (filter?.GenreId is { } genre)
+                query = query.Where(p => p.GenreId == genre);
+
+            if (filter?.AuthorId is { } author)
+                query = query.Where(p => p.AuthorId == author);
+
+            return query;
+        }
+    }
+}
