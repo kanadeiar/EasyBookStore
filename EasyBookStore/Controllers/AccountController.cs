da792ba8f144620e9d162208df3debae65e4245f
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-26 11:49:38 +0400

    Создание контроллера авторизации пользователей

---
+++ EasyBookStore/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyBookStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
--- EasyBookStore/Startup.cs
+++ EasyBookStore/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using EasyBookStore.Dal.Context;
 using EasyBookStore.Data;
 using EasyBookStore.Domain.Models.Identity;
@@ -12,7 +13,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
 
 namespace EasyBookStore
 {
