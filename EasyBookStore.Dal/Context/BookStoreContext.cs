063aa2d73d7971ebc19268f61679ffdf79a05a2a
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-26 11:25:51 +0400

    Добавление идентификации в базу данных и миграции

--- EasyBookStore.Dal/Context/BookStoreContext.cs
+++ EasyBookStore.Dal/Context/BookStoreContext.cs
@@ -5,22 +5,12 @@ using EasyBookStore.Domain.Models.Identity;
 
 namespace EasyBookStore.Dal.Context
 {
-    //public class BookStoreContext : IdentityDbContext<User, Role, string>
-    //{
-    //    public DbSet<Genre> Genres { get; set; }
-    //    public DbSet<Author> Authors { get; set; }
-    //    public DbSet<Product> Products { get; set; }
-
-    //    public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
-
-    //}
-    public class BookStoreContext : DbContext
+    public class BookStoreContext : IdentityDbContext<User, Role, string>
     {
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Product> Products { get; set; }
 
         public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
-
     }
 }
