c13fc9228a86c17491b432cac2083e798fbba5de
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-26 17:19:28 +0400

    Расширение фильра данных товаров для поддержки запросов по идентификаторам

--- EasyBookStore.Domain/Common/ProductFilter.cs
+++ EasyBookStore.Domain/Common/ProductFilter.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EasyBookStore.Domain.Common
+﻿namespace EasyBookStore.Domain.Common
 {
     /// <summary> Фильтр товаров </summary>
     public class ProductFilter
@@ -13,5 +7,7 @@ namespace EasyBookStore.Domain.Common
         public int? GenreId { get; set; }
         /// <summary> Фильтр по авторам книг </summary>
         public int? AuthorId { get; set; }
+        /// <summary> Идентификаторы товаров, которые нужно показать </summary>
+        public int[] Ids { get; set; }
     }
 }
--- EasyBookStore/Services/Database/DatabaseProductData.cs
+++ EasyBookStore/Services/Database/DatabaseProductData.cs
@@ -3,6 +3,7 @@ using EasyBookStore.Domain.Common;
 using EasyBookStore.Domain.Models;
 using EasyBookStore.Interfaces.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,9 +12,12 @@ namespace EasyBookStore.Services.Database
     public class DatabaseProductData : IProductData
     {
         private readonly BookStoreContext _context;
-        public DatabaseProductData(BookStoreContext context)
+        private readonly ILogger<DatabaseProductData> _logger;
+
+        public DatabaseProductData(BookStoreContext context, ILogger<DatabaseProductData> logger)
         {
             _context = context;
+            _logger = logger;
         }
         public IEnumerable<Genre> GetGenres()
         {
@@ -35,12 +39,19 @@ namespace EasyBookStore.Services.Database
         {
             IQueryable<Product> query = _context.Products;
 
-            if (filter?.GenreId is { } genre)
-                query = query.Where(p => p.GenreId == genre);
+            if (filter?.Ids?.Length > 0)
+            {
+                query = query.Where(p => filter.Ids.Contains(p.Id));
+            }
+            else
+            {
+                if (filter?.GenreId is { } genre)
+                    query = query.Where(p => p.GenreId == genre);
 
-            if (filter?.AuthorId is { } author)
-                query = query.Where(p => p.AuthorId == author);
-
+                if (filter?.AuthorId is { } author)
+                    query = query.Where(p => p.AuthorId == author);
+            }
+            _logger.LogInformation($"SQL: {query.ToQueryString()}");
             return query;
         }
 
