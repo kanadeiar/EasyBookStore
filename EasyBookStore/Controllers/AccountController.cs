b7b620eef419f635560c7c7a7784b3e051340b6f
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-26 11:51:48 +0400

    внедрение методов системы идентификации

--- EasyBookStore/Controllers/AccountController.cs
+++ EasyBookStore/Controllers/AccountController.cs
@@ -1,9 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EasyBookStore.Domain.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EasyBookStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         public IActionResult Register()
         {
             return View();
