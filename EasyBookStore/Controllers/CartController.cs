247199ebb4d9bf33a29805a8258b122a35bee390
kanadeiar <kanadeiar@gmail.com>
2021-10-28 03:13:59 +0400

    Корзина работает

---
+++ EasyBookStore/Controllers/CartController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyBookStore.Interfaces.Services;
+
+namespace EasyBookStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _cartService.GetWebModel();
+            return View(model);
+        }
+
+        public IActionResult Add(int id)
+        {
+            _cartService.Add(id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Decrement(int id)
+        {
+            _cartService.Decrement(id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Remove(int id)
+        {
+            _cartService.Remove(id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Clear()
+        {
+            _cartService.Clear();
+            return RedirectToAction("Index", "Cart");
+        }
+    }
+}
