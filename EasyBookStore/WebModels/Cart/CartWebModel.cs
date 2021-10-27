27ecd0564d3cae6e383731fd36cf5e5ff4748c6d
kanadeiar <kanadeiar@gmail.com>
2021-10-28 01:18:16 +0400

    Вебмодель корзины

--- EasyBookStore/WebModels/Cart/CartWebModel.cs
+++ EasyBookStore/WebModels/Cart/CartWebModel.cs
@@ -1,11 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace EasyBookStore.WebModels.Cart
 {
     public class CartWebModel
     {
+        public IEnumerable<(ProductWebModel Product, int Quantity, decimal PriceSum)> Items { get; set; }
+        public int ItemsSum => Items?.Sum(p => p.Quantity) ?? 0;
+        public decimal PriceSum => Items?.Sum(p => p.Product.Price * p.Quantity) ?? 0;
     }
 }
