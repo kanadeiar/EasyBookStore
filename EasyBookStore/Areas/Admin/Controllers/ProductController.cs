28858cbcb8f077d52e81a83be7357aac72078385
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-30 10:38:55 +0400

    Товары отображаются в разделе редактирования товаров

--- EasyBookStore/Areas/Admin/Controllers/ProductController.cs
+++ EasyBookStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,6 +1,11 @@
 ﻿using EasyBookStore.Domain.Models.Identity;
+using EasyBookStore.Interfaces.Services;
+using EasyBookStore.Services;
+using EasyBookStore.WebModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
 
 namespace EasyBookStore.Areas.Admin.Controllers
 {
@@ -8,9 +13,19 @@ namespace EasyBookStore.Areas.Admin.Controllers
     [Authorize(Roles = Role.Administrators)]
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductData _productData;
+        private readonly IMapper<ProductWebModel> _mapper;
+
+        public ProductController(IProductData productData, IMapper<ProductWebModel> mapper)
         {
-            return View();
+            _productData = productData;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productData.GetProductsAsync();
+            return View(products.Select(p => _mapper.Map(p)));
         }
     }
 }
