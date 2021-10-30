74545616cfe770b80d30c4f1a663c9d4f7514b0b
kanadeiar <42916202+kanadeiar@users.noreply.github.com>
2021-10-30 23:08:02 +0400

    Расширение базы данных

--- EasyBookStore.Dal/Migrations/20211030190538_order.Designer.cs
+++ EasyBookStore.Dal/Migrations/20211030190538_order.Designer.cs
@@ -4,14 +4,16 @@ using EasyBookStore.Dal.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EasyBookStore.Dal.Migrations
 {
     [DbContext(typeof(BookStoreContext))]
-    partial class BookStoreContextModelSnapshot : ModelSnapshot
+    [Migration("20211030190538_order")]
+    partial class order
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
